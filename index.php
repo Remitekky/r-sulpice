2a29d84366cd930dc9b10b210613bdec6df9ded0
Sulpice Remi <remitekky@hotmail.fr>
2020-07-05 23:25:33 +0200

    Nav & Home basic structure

---
+++ index.php
@@ -0,0 +1,42 @@
+<!doctype html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>R-Sulpice</title>
+    <link rel="stylesheet" href="styles/css/style.css">
+</head>
+<body>
+    <div class="main-wrapper">
+        <div class="navigation-wrapper">
+            <nav>
+                <div class="brand-logo">Brand Logo</div>
+                <ul>
+                    <li>Home</li>
+                    <li>Presentation</li>
+                    <li>Skills</li>
+                    <li>Works</li>
+                    <li>Contact</li>
+                </ul>
+            </nav>
+        </div>
+        <div class="content-wrapper">
+            <div class="home-wrapper">
+                <div class="home-content">
+                    <div class="intro-text">
+                        <h1>Rémi Sulpice</h1>
+                        <p>Hi ! I'm Rémi.</p>
+                        <p>A Fullstack Web Developer.</p>
+                        <p>I mainly work in Php, Javascript, Prestashop and Symfony technologies</p>
+                    </div>
+                    <div class="intro-photo">
+                        <img src="styles/img/personnal-photo.JPG" alt="personnal-photo">
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</body>
+</html>
